2ae36231a5f7b254a0cad659b36f16153ea6fc5c
Roman Klimov <timeflieswhenyouhf@internet.ru>
2023-07-13 23:34:09 +0300

    alpha

--- server/src/alpha/core/index.ts
+++ server/src/alpha/core/index.ts
@@ -1,34 +1,64 @@
+import arrayShuffle from 'array-shuffle';
+
 abstract class Hero {
-    id: string
+    id: number
+    name: string
     hits: number
+    squad: string
+    isConnected: boolean
+    turn: boolean
 }
 
-abstract class Game {
-    heroes: Hero[]
+class Game {
+    heroes: HeroSchema
+    schema: CardSchema
 }
 
-type CardSchema = Record<string, {name: string, owner: string | null, deck: string}>
-const proxy = new Proxy<CardSchema>({}, {
-    set(target, p, newValue, receiver) {
-        console.log(target)
-        return newValue
-    },
-})
-// card schema
-// id       name        owner       deck
-// euu3y    dice_twins  climoy      hand
-// 0003e    sword       climoy      equip
-// 00e32    brothers    null        loots
-// 32eed    magic_hat   null        draws
-// 003aq    curse       climoy      skirmish
+type Blueprint = {group: Group}
+type Blueprints = Record<string, Blueprint>
 
-class CoresKit {
-    find_hero_by_id(options: {heroes: Hero[], id: string}): Hero 
-    { return options.heroes.find((hero) => hero.id === options.id) }
+type Deck = 'loots' | 'doors' | 'onboard' | 'inhand' | 'inuse' | 'areplayed' | 'stashed'
+type Group = 'bonus' | 'creature' | 'effects' | 'attributes' | 'battlebuffs'
+class HeroSchema {
+    constructor(protected schema: Array<Hero>){
+        this.initiator = schema[0]
+    }
+    protected total: number
+    protected _initiator: Hero
+    set initiator(hero: Hero) { this._initiator = hero }
+    get initiator() { return this._initiator }
+    check_online() { this.initiator.isConnected ? {} : this.pass_init() }
+    pass_init() { this.initiator = this.initiator.id > this.total - 1 ? this.schema[this.initiator.id + 1] : this.schema[0] }
+    find_name(name: string) { this.schema.find((hero) => hero.name === name) }
+}
+class CardSchema {
+    protected schema: Record<string, {name: string, owner: string | null, deck: Deck}>
+    constructor(protected bp: Blueprints){}
+    transfer(id: string, owner: string) { if (this.schema.table[id]) {this.schema.table[id][1] = owner} }
+    equip(id: string) { this.schema.table[id][2] = 'equiped' }
+    stash(id: string) { this.schema.table[id][2] = 'stashed' }
+    get loots() {  return arrayShuffle(Object.entries(this.schema).filter((row) => row[1].deck === 'loots')) }
+    get doors() {  return arrayShuffle(Object.entries(this.schema).filter((row) => row[1].deck === 'doors')) }
+    take_top(num: number, deck: 'loots' | 'doors') 
+    { if (this[deck].length >= num) { this[deck].splice(0, num) } else {
+        this.restore_deck(deck)
+        this.take_top(num, deck)
+    } }
+    restore_deck(deck: 'loots' | 'doors')
+    { Object.values(this.schema)
+        .map((row) => row.deck === 'stashed' 
+        ? this.bp[row.name].group === 'bonus' 
+        ? row.deck = 'loots' 
+        : row.deck = 'doors'
+        : {}
+    )}
+}
+
+class Cores {
+    constructor(protected bp: Blueprints){}
+    card_play(id: string) {  }
     deal_damage(options: {hits: number, damagable: {hits: number}}) 
     { options.damagable.hits -= options.hits }
     increase_rank(options: {rankable: {rank: number}}) 
     { options.rankable.rank ++ }
-    transfer_card(options: {id: string, schema: CardSchema, owner: string | null, deck: string})
-    { options.schema[options.id] = {...options.schema[options.id], owner: options.owner, deck: options.deck} }
 }
